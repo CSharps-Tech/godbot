42088c2d6228214a55a43b98ec5a24d0fa4bb93f
CSharps-Tech <114924195+CSharps-Tech@users.noreply.github.com>
2022-12-18 16:21:12 +0700

    Update shayo_boot.py

--- shayo/shayo_boot.py
+++ shayo/shayo_boot.py
@@ -5,3 +5,12 @@ try:
    shayo.connect(connect)
    # token
    shayo.login(shayo.get[i].token())
+if shayo.error==true:
+   print("We got an error:" + shayo.errors + "\nYou send e-mail to shayo@godhub.com to support")
+if shayo.perm==true:
+   print("We got an permission error from " + shayo.perms + "\nWe send the logs to developer shayo now")
+   shayo.send(shayo.perms)
+if shayo.perk == "expired":
+   print("You bot expired perk II, Please wait for moment to rebuy perk again")
+if shayo.build == "bx":
+   print("Building you bot, you have received administration mode to edit/build")
