a95a2e0407895ffe0ec1f9155c6710952ef8c60a
CSharps-Tech <114924195+CSharps-Tech@users.noreply.github.com>
2022-12-18 12:01:37 +0700

    Do not edit

---
+++ shayo/shayo_boot.py
@@ -0,0 +1,7 @@
+# Do not edit the following link
+import shayo
+try:
+   connect = "https://shayo.cps.com:7075/app?client=69547"
+   shayo.connect(connect)
+   # token
+   shayo.login(shayo.get[i].token())
